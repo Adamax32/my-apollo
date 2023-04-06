0dfb521e939dcceec34998d5d4d5230615ee574e
Adamax32 <adamaxcisse@gmail.com>
2023-04-06 17:23:43 -0400

    Part4 : CSS Styling to the FILMS PAGE

--- src/modules/films/FilmList.js
+++ src/modules/films/FilmList.js
@@ -14,6 +14,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { GET_ALL_FILMS } from "./queries";
 
+// Import Parisienne font from Google Fonts
+import "@fontsource/parisienne";
+
 export const FilmList = () => {
   const { data, error, loading } = useQuery(GET_ALL_FILMS);
 
@@ -29,45 +32,49 @@ export const FilmList = () => {
         <Grid item key={i} xs={25}>
           <Card>
             <CardContent>
-            <Typography variant="h4" style={{ fontFamily: 'Arial', fontSize: '16px' }}>
-              {film.title}
-            </Typography>
-            <Typography variant="subtitle" style={{ fontFamily: 'Arial', fontSize: '14px' }}>
-              Release Date: {film.releaseDate}
-            </Typography>
-            <Typography variant="h2" style={{ fontFamily: 'Arial', fontSize: '14px' }}>
-              Director: {film.director}
-            </Typography>
-            <Typography>
-              Characters:
-            </Typography>
+              {/* Use Parisienne font for title */}
+              <Typography variant="h4" style={{ fontFamily: 'Parisienne', fontSize: '45px' }}>
+                {film.title}
+              </Typography>
+              {/* Use Parisienne font for subtitle */}
+              <Typography variant="subtitle" style={{ fontFamily: 'Parisienne', fontSize: '32px' }}>
+                Release Date: {film.releaseDate}
+              </Typography>
+              {/* Use Parisienne font for director */}
+              <Typography variant="h2" style={{ fontFamily: 'Parisienne', fontSize: '28px' }}>
+                Director: {film.director}
+              </Typography>
+              <Typography>
+                Characters:
+              </Typography>
 
-            <Accordion>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h2" style={{ fontFamily: 'Arial', fontSize: '14px' }}>
-                  View Characters
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <ul>
-                  {film.characterConnection.characters.map((character) => (
-                    <li key={character.name}>
-                      <Typography variant="h2" style={{ fontFamily: 'Arial', fontSize: '14px' }}>
-                        {character.name} : from {character.homeworld.name}
-                      </Typography>
-                      <CardActions>
-                        <Link to={`/films/${character.id}`}>Check me Out!</Link>
-                      </CardActions>
-                    </li>
-                  ))}
-                </ul>
-              </AccordionDetails>
-            </Accordion>
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  {/* Use Parisienne font for accordion summary */}
+                  <Typography variant="h2" style={{ fontFamily: 'Parisienne', fontSize: '22px', color: 'red' }}>
+                    View Characters
+                  </Typography>
+
+
+                </AccordionSummary>
+                <AccordionDetails>
+                  <ul>
+                    {film.characterConnection.characters.map((character) => (
+                      <li key={character.name}>
+                        {/* Use Parisienne font for character name */}
+                        <Typography variant="h2" style={{ fontFamily: 'Parisienne', fontSize: '22px' }}>
+                          {character.name} : from {character.homeworld.name}
+                        </Typography>
+                        <CardActions>
+                          <Link to={`/films/${character.id}`}>Check me Out!</Link>
+                        </CardActions>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionDetails>
+              </Accordion>
 
             </CardContent>
-            {/* <CardActions>
-              <Link to={`/films/${film.id}`}>more info</Link>
-            </CardActions> */}
           </Card>
         </Grid>
       ))}
--- src/pages/FilmsPage.js
+++ src/pages/FilmsPage.js
@@ -2,15 +2,31 @@ import { Grid, Typography } from "@mui/material";
 import React from "react";
 import { FilmList } from "../modules/films/FilmList";
 
+const backgroundImage = "url('https://spaceplace.nasa.gov/nebula/en/nebula1.en.jpg')";
+
 export const FilmsPage = () => {
   return (
-    <Grid container spacing={2} style={{ backgroundColor: 'lightblue' }}>
-      <Grid item xs={10}>
-        <Typography variant="h3">Films</Typography>
+    <div
+      style={{
+        backgroundImage,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        width: '80vw', // Add this line to set the width
+        margin: '0 auto', // Add this line to center the container horizontally
+      }}
+    >
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        <Grid item xs={10}>
+          <Typography variant="h3" style={{ fontFamily: 'Parisienne', fontSize: '99px', color: '#fff', textAlign: 'center' }}>
+            Films
+          </Typography>
+        </Grid>
+        <Grid item xs={10} sx={{ marginLeft: '50px' }}>
+          <FilmList />
+        </Grid>
       </Grid>
-      <Grid item xs={10}>
-        <FilmList />
-      </Grid>
-    </Grid>
+
+    </div>
   );
 };
